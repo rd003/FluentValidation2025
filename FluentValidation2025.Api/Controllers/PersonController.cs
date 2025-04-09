ad856685c5c8a04f54a47ce0f5dc6908711dc665
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-04-09 12:39:45 +0530

    feat: base setup ready

---
+++ FluentValidation2025.Api/Controllers/PersonController.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation2025.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidation2025.Api.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddPerson(Person person)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ FluentValidation2025.Api/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FluentValidation2025.Api.Models;
+
+public class Person
+{
+    [Required]
+    public int? Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string? Name { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    [EmailAddress]
+    
+    public string? Email { get; set; }
+    [Required]
+    
+    public int? Age { get; set; }
+}
